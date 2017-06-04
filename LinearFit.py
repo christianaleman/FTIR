5ba32b77cb426e7295f1a4acc4e9d7adeeaa53be
Alexander Aleman <alexander.aleman@gmail.com>
2017-06-04 09:20:27 +0200

    Use bisect for getting location in list

--- LinearFit.py
+++ LinearFit.py
@@ -1,25 +1,17 @@
+import bisect
+
+
 def Linearfit(molecule_concentrations, molecule_spectra):
     new_spectra_list = {}
-    low_value_calibrationlist = 0
-    low_value = 0
-    high_value_calibrationlist = 0
-    high_value = 0
     for key,value in molecule_concentrations.items():
         new_spectra_list_per_component ={}
         kalibratielijst = molecule_spectra[key][1]
 
-        if value > kalibratielijst[len(kalibratielijst) - 1]:
-            low_value_calibrationlist = kalibratielijst[len(kalibratielijst) - 2]
-            low_value = len(kalibratielijst) - 2
-            high_value_calibrationlist = kalibratielijst[len(kalibratielijst) - 1]
-            high_value = len(kalibratielijst) - 1
-        else:
-            for calibrationfile in range(0,len(kalibratielijst)):
-               if value >= kalibratielijst[calibrationfile]:
-                    low_value_calibrationlist = kalibratielijst[calibrationfile]
-                    low_value = calibrationfile
-                    high_value_calibrationlist = kalibratielijst[calibrationfile+1]
-                    high_value = calibrationfile + 1
+        i = bisect.bisect(kalibratielijst, value)
+        low_value = i - 1 if i < len(kalibratielijst) else i - 2
+        high_value = i if i < len(kalibratielijst) else i - 1
+        low_value_calibrationlist = kalibratielijst[low_value]
+        high_value_calibrationlist = kalibratielijst[high_value]
 
         for iter1, iter2 in molecule_spectra[key][0].items():
             slope = (float(iter2[high_value])-float(iter2[low_value]))/(float(high_value_calibrationlist)-float(low_value_calibrationlist))
